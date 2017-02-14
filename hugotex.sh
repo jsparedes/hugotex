6e44293e28242a945a0a59513bcddbe57f098fef
Jorge Paredes <warderm6@gmail.com>
2017-02-13 21:36:59 -0500

    Load bibtex file into cv

--- hugotex.sh
+++ hugotex.sh
@@ -5,3 +5,5 @@ go run multi.go
 go run rmblankspace.go
 cd public/
 find . -type f -name "*.tex" -exec xelatex {} \;
+find . -type f -name "*.aux" -exec bibtex {} \;
+find . -type f -name "*.tex" -exec xelatex {} \;
